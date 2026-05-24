0dad740ca11a1334d200189e01aee884477559ac
rohith <rohk002@gmail.com>
2026-05-24 17:54:47 +0530

    delete api completed

--- Backend/App/routers/users_route.py
+++ Backend/App/routers/users_route.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from App.config.db_connection import get_db
 from App.schema.users_schema import UserListWrapper, UserResponse, CreateUser, LoginResponse , LoginRequest
-from App.service.users_service import create_user , list_users , login_user
+from App.service.users_service import create_user , list_users , login_user , delete_user_service
 from App.service.authentication_service import get_current_user
 
 router = APIRouter(tags=["user management"])
@@ -12,9 +12,13 @@ async def new_user(user: CreateUser, db: Session= Depends(get_db)):
     return create_user(user,db)
 
 @router.get("/list-all-users", response_model=UserListWrapper)
-async def list_all_users(db: Session= Depends(get_db), current_user=Depends(get_current_user)):
+async def list_all_users(db: Session= Depends(get_db)):
     return list_users(db)
 
 @router.post("/login", response_model=LoginResponse)
 async def login_service(user:LoginRequest, db: Session= Depends(get_db)):
     return login_user(user, db)
+
+@router.delete("/delete-user/{user_id}", response_model=UserResponse)
+async def delete_user(user_id: str, db: Session= Depends(get_db)):
+    return delete_user_service(user_id,db)
--- Backend/App/service/users_service.py
+++ Backend/App/service/users_service.py
@@ -75,3 +75,18 @@ def login_user(user_data: LoginRequest, db: Session):
         "token": token,
         "expired_at": expired_at
     }
+
+def delete_user_service(user_id: str, db: Session):
+    user= db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+    
+    db.delete(user)
+    db.commit()
+    return {
+        "id": str(user.id),
+        "message": "User deleted successfully"
+    }
